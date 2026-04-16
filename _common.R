d111677ea4aa85ae6eade66f1ac964c0cb265b96
Timothy Dobbins <t.dobbins@unsw.edu.au>
2026-04-16 14:51:42 +1000

    Convert tables to tinytable - Module 1

--- _common.R
+++ _common.R
@@ -5,10 +5,13 @@ library(dplyr)
 # Theme for gt tables
 theme_bt <- function(data, ...){
     data |> 
-    tab_options(table_body.hlines.color = "transparent",
+    tab_options(table.font.size = px(12),
+                table_body.hlines.color = "transparent",
                 column_labels.border.top.color = "black",
                 column_labels.border.bottom.color = "black",
                 table_body.border.bottom.color = "black",
+                data_row.padding = px(1.5),
+                row.striping.include_table_body = FALSE,
      ...
       )
   }
